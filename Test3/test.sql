767ea62f63dde576fb9a6de733b57383c5614411
Andrey Bobrov <andrey.bobrov.irk@gmail.com>
2019-11-29 03:15:53 +0800

    №3

---
+++ Test3/test.sql
@@ -0,0 +1,3 @@
+﻿CREATE PROCEDURE test3 @id nvarchar(30), @path nvarchar(100), @type nvarchar(20), @new_val nvarchar(100) 
+AS
+  EXEC('update [dbo].[Table] set field1.modify(''replace value of (/' + @path + '/text())[1] with "' + @new_val + '" cast as ' + @type +' ?'') where Id=' + @id)
